f803bc3fe3eb9ad6d9d36ea40dc7e0ae8d1c40bd
Deepak.Rohila <deepak@ubuntu.ubuntu-domain>
2016-12-20 03:01:16 +0530

    Setting flag learning to true

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -170,7 +170,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent)
+    agent = env.create_agent(LearningAgent,learning = True)
     
     ##############
     # Follow the driving agent
